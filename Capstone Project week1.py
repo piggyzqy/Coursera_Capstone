889e62d1c6831b957b4401edc611a398fdf0fd6b
piggyzqy <56534600+piggyzqy@users.noreply.github.com>
2019-10-14 16:17:01 +0800

    Add files via upload

---
+++ Capstone Project week1.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# ** This Notebook will be mainly used for Coursera Capstone Final Project. **
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print ("Hello Capstone Project Course!")
+
